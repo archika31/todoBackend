8cc0c14edc18934fdf8f1229405d4d8e4b6963df
archika <archikakasliwal31@gmail.com>
2024-04-04 15:18:54 +0530

    changes are done

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
 
-const connectDB=mongoose.connect("mongodb://0.0.0.0:27017/ToDo").then(()=>{
-    console.log("database connection")
-}).catch((err)=>{
-    console.log(err)
-})
+const connectDB = ()=>{
+   mongoose.connect(process.env.CONNECTION_STRING).then((c)=>{
+    console.log(`database connected with ${c.connection.host}`)
+   })
+    .catch((err)=>{
+        console.log(err)
+    })}
 
 export default connectDB;
